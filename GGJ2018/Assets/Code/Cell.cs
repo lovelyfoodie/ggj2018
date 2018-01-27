ddaa79dee934fb36d16225e14322e1c917e4e083
EnkiduRed <rbrolley@alchemedium.com>
2018-01-27 15:52:51 -0500

    added max velocity

--- GGJ2018/Assets/Code/Cell.cs
+++ GGJ2018/Assets/Code/Cell.cs
@@ -9,17 +9,30 @@ public class Cell : MonoBehaviour
 {
     public float startingTorque = 1f;
     public float maxSpeed = 1f;
+
     private Rigidbody2D _rb;
-    
+    private float _sqrMagMaxSpeed;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _rb.AddTorque(Random.Range(0, startingTorque));
-    }
-    
-    private void Update()
-    {
 
+        _sqrMagMaxSpeed = maxSpeed * maxSpeed;
+    }
+
+    private void OnValidate()
+    {
+        _sqrMagMaxSpeed = maxSpeed * maxSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rb.velocity.sqrMagnitude > _sqrMagMaxSpeed)
+        {
+            var exceed = _rb.velocity.sqrMagnitude - _sqrMagMaxSpeed;
+            _rb.AddForce(_rb.velocity.normalized * -1f * Mathf.Sqrt(exceed));
+        }
     }
 
     public void AddForce(Vector3 force)
