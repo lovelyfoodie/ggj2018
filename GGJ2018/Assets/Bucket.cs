d0f4045c204d6002570627eca5173bb5c0012d4e
lovelyfoodie <bonnieray12@gmail.com>
2018-01-27 15:34:40 -0500

    added bucket color change for playah 1

---
+++ GGJ2018/Assets/Bucket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Bucket : MonoBehaviour {
+
+	//TODO: Take in gate collider object in the 
+
+	private Color BLUE = Color.blue;
+	private Color RED = Color.red;
+	private bool isBlue = false;
+
+	public GameObject gate;
+	private SpriteRenderer gateSprite;
+
+
+	// Use this for initialization
+	void Start () {
+		gateSprite = gate.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") && isBlue) {
+			gateSprite.color = RED;
+			gate.layer = 8;
+			isBlue = false;
+		} else if(Input.GetButtonDown ("Fire1") && !isBlue) {
+			gateSprite.color = BLUE;
+			gate.layer = 9;
+			isBlue = true;
+		}
+		
+	}
+}
