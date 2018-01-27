6aaa1f0eacf7cb9a12654530ea2c0e239d9f4ac2
EnkiduRed <rbrolley@alchemedium.com>
2018-01-27 15:42:46 -0500

    merge fix

--- GGJ2018/Assets/Code/Cell.cs
+++ GGJ2018/Assets/Code/Cell.cs
@@ -8,13 +8,18 @@ using UnityEngine;
 public class Cell : MonoBehaviour
 {
     public float startingTorque = 1f;
+    public float maxSpeed = 1f;
     private Rigidbody2D _rb;
-
-
+    
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _rb.AddTorque(Random.Range(0, startingTorque));
+    }
+    
+    private void Update()
+    {
+
     }
 
     public void AddForce(Vector3 force)
